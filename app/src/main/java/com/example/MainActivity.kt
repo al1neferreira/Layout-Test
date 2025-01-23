3c252f124c184252b10ea36e57be615930fe452b
al1neferreira <alinfst@gmail.com>
2025-01-23 20:26:37 -0300

    Add color button function

--- app/src/main/java/com/example/MainActivity.kt
+++ app/src/main/java/com/example/MainActivity.kt
@@ -6,16 +6,50 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.layout_test.R
+import com.example.layout_test.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        onView()
+
+    }
+
+    private fun onView() {
+        colorButton(true)
+    }
+
+    private fun colorButton(isColored: Boolean) {
+        var botao = binding.btnButton
+        if (isColored) {
+            botao.setBackgroundColor(
+                resources.getColor(
+                    R.color.purple_500,
+                    null
+                )
+            )
+        } else {
+            botao.setBackgroundColor(
+                resources.getColor(
+                    R.color.black,
+                    null
+                )
+            )
+        }
     }
 }
+
